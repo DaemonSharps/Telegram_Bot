f561d6582d3413a497cd8481d525265dcae43c44
DaemonSharps <56834210+DaemonSharps@users.noreply.github.com>
2020-10-06 20:08:41 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$TOKEN = '1232684481:AAF-H0BnX_xQaxWKisaiHodklQLZTffKLYo'; //ваше токен, получен в BotFather
-$BOT_USERNAME = 'DeliveryCourierServiceBot'; // юзернейм бота (без собачки)
-$webhook = 'https://body-in-12-days.xyz/MC-money-counter/Money.php'; //ваша ссылка
+$TOKEN = '************************'; //ваше токен, получен в BotFather
+$BOT_USERNAME = '*************************'; // юзернейм бота (без собачки)
+$webhook = 'https://************************'; //ваша ссылка
 $url = 'https://api.telegram.org/bot'.$TOKEN.'/';
 ?>
